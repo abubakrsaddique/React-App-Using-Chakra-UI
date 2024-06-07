46d412f8b11a4df498b851f187b482015be6f1b2
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-06-07 16:21:28 +0500

    Responsive 1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Header from './components/header/Header' 
-import Navbar from './components/navbar/Navbar'
-import RealEstateSection from './components/realestatesection/RealEstateSection'
-import CardComponent from './components/cardcomponent/CardComponent'
-import MarqueeComponent from './components/marqueecomponent/MarqueeComponent'
-import About from './components/About/About'
+// import Navbar from './components/navbar/Navbar'
+// import RealEstateSection from './components/realestatesection/RealEstateSection'
+// import CardComponent from './components/cardcomponent/CardComponent'
+// import MarqueeComponent from './components/marqueecomponent/MarqueeComponent'
+// import About from './components/About/About'
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Navbar/>
+      {/* <Navbar/>
       <RealEstateSection />
       <CardComponent />
-      <MarqueeComponent />
-      <About/>
+      <MarqueeComponent /> */}
+      {/* <About/> */}
     </div>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Box, Flex, Text, Icon } from '@chakra-ui/react';
+import { Box, Flex, Text, Icon, useMediaQuery } from '@chakra-ui/react';
 import { MdLocationOn, MdPhone, MdEmail } from 'react-icons/md';
 
 const Header = () => {
+  const [isLargerThanMD] = useMediaQuery('(min-width: 768px)');
+
+  if (!isLargerThanMD) {
+    return null;
+  }
+
   return (
     <Box bg="#3a0ca3" color="white" p={2} position="absolute" top={0} left={0} w="100%">
       <Flex justify="space-between" align="center">
@@ -11,8 +17,8 @@ const Header = () => {
           <Text ml={1} fontSize="14px">Rezilla, 18 Grattan St, Brooklyn</Text>
         </Flex>
 
-        <Flex align="center"  mr="50px">
-          <Flex align="center" >
+        <Flex align="center" mr="50px">
+          <Flex align="center">
             <Icon as={MdPhone} w={5} h={5} />
             <Text ml="10px" fontSize="14px">+1 206-214-229</Text>
           </Flex>
